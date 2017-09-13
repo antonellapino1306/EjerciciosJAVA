4ce94b2a5bca6284fa9580b1d848a7576b2ea6e3
antonellapino1306 <31899215+antonellapino1306@users.noreply.github.com>
2017-09-13 18:00:47 -0300

    Commit EjercicioSiete

---
+++ EjercicoSiete.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicosiete;
+
+/**
+ *
+ * @author alumno
+ */
+public class EjercicoSiete {
+
+    /**
+     * @param args the command line arguments
+     * se pide la vase de un cuadrado y se informa el perimetro
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
