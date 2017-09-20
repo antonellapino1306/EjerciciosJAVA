22502c74d5b754762ec0b621f3adda9e4a579fa0
antonellapino1306 <31899215+antonellapino1306@users.noreply.github.com>
2017-09-20 18:01:46 -0300

    Add files via upload

---
+++ ProyectoUno.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectouno;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author alumno
+ */
+public class ProyectoUno {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner miSc=new Scanner (System.in);
+        //dato variables
+        String Nombre;
+        String Clave;
+        String Sexo;
+        String Localidad;
+        String Colorfavorito;
+        
+        
+        
+        
+        
+        //tomo clave
+        System.out.println("ingrese nombre:");
+        Nombre=miSc.next();
+        
+        
+        
+        System.out.println("ingrese clave");
+        Clave= miSc.next();
+        
+        
+        System.out.println("ingrese el sexo");
+        Sexo= miSc.next(); 
+        
+        
+        System.out.println("ingrese localidad");
+        Localidad= miSc.next();
+        
+        
+        System.out.println("ingrese el color");
+        Colorfavorito= miSc.next();
+        
+        
+        
+        
+        //si es masculino que viva en lanus o en avellaneda y que su color favorito sea el rojo mostrar el mesajesos de independiente
+        //si el nombre es andrea y localidad moron y el color favorito es verdeaguaazuz se le pone mensaje feliz primavera
+        
+        
+       
+        
+           if(Colorfavorito.equals("blanco")|| Colorfavorito.equals("celeste")
+        
+             System.out.println("patriota");
+            {
+             if(Colorfavorito.equals("rojo"))
+                 
+                System.out.println("sos de independiente");
+            }
+            
+            if(Sexo.equals("m"))
+                
+            else
+             
+             System.out.println("f");
+            
+            
+            
+            
+            
+                
+                
+        
+       
+        
+    //          if ( localidad.equals("lanus");
+    //          (
+    //          if (sexo.equals("f))
+    //
+    //          (                             
+    //              System.out.printIn("Bienvenidos")
+    //           }
+    //
+    //
+    //
+        
+        
+        
+            if(Localidad.equals("lanus") Sexo.equals("f") ) 
+      
+        
+        
+        
+              System.out.println("Bienvenido");
+        }
+      
+              System.out.println("Bienvenido"+ Nombre);
+        
+        
+        
+        
+        
+             
+       
+       
+        
+    }
+    }
+    
+}
