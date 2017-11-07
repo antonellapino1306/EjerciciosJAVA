c9b4151c216921c5c91bd696f98783009f2f953c
antonellapino1306 <31899215+antonellapino1306@users.noreply.github.com>
2017-11-07 17:57:45 -0300

    EjemplosClaseYObjetos

---
+++ EjemplosClaseYObjetos/src/EjemplosClaseYObjetos/EjemplosClaseYObjetos.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EjemplosClaseYObjetos;
+
+/**
+ *
+ * @author alumno
+ */
+public class EjemplosClaseYObjetos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //int Edad;
+        
+        //esto es de instacia
+        //Calculadora misCal = new Calculadora();
+        //miCal.MostrarInstancia();
+        int Numero;
+        String Datos;
+        Alumno nuevoAlumno
+        
+        
+    }
+    
+}
