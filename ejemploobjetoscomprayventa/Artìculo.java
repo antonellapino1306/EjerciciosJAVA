60e9462511e05a0a5e27fa62cd8233c72609951a
antonellapino1306 <31899215+antonellapino1306@users.noreply.github.com>
2017-11-28 17:53:51 -0300

    /ejemploobjeto

---
+++ ejemploobjetoscomprayventa/Artìculo.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemploobjetoscomprayventa;
+
+/**
+ *
+ * @author alumno
+ */
+public class Artìculo {
+  private int _codigo;
+  private String _nombre;
+  private float _precioCosto;
+  private float _precioVenta;
+  private int stock;
+  
+  public float GetterPrecioVenta ()
+  {
+      return this._precioVenta;
+  }
+  String Retorno;
+  
+ public String GetterNombreYc
+  
+  
+  
+  
+  public void 
+ /*
+ Además debe tener los getter de PrecioVenta y NombreyCodigo.
+Los setter de precio costo y stock
+Constructor que reciba todos los atributos por parámetros menos el precio venta que se
+calcula con un 25% de incremento sobre el precio de costo.
+Métodos:
+Instancia
+HayStock :bool
+Static
+SonIguales(articulo uno articulo dos) :bool Compara el nombre y el código.
+
+*/
+
+
+
+
+
+
+}
