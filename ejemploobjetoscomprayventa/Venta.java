a165570b7a02f293878b6c2625b12082eba5c660
antonellapino1306 <31899215+antonellapino1306@users.noreply.github.com>
2017-11-29 15:00:35 -0300

    Update Venta.java

--- ejemploobjetoscomprayventa/Venta.java
+++ ejemploobjetoscomprayventa/Venta.java
@@ -11,4 +11,66 @@ package ejemploobjetoscomprayventa;
  */
 public class Venta {
     
+private articulo _articulovendido;
+    private int _cantidad;
+    
+    
+    public void MostrarVenta(){
+        
+        System.out.println("Precio cantidad : "+this._cantidad);
+        System.out.println("Producto : ");
+        this._articulovendido.MostrarArticulo();
+    }
+    
+    public Venta(articulo vendido,int cantidad){
+        this._articulovendido=vendido;
+        this._cantidad=cantidad;
+       
+      
+    }
+    
+    
+    public float RetornarGanancia(){
+        
+        float precioFacturado;
+        precioFacturado=this._articulovendido.GetterGanancia()*this._cantidad;
+        return precioFacturado;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+//    public Articulo GetVendido(){
+//        
+//        Articulo retorno=this.vendido;
+//        
+//        return retorno;
+//    }
+//    
+//    public void SetterProdVendido(Articulo prodvendido){
+//        this.vendido=prodvendido;
+//    }
+//   
+//    public int GetCantidad(){
+//        
+//        int retorno=this.cantidad;
+//        
+//        return retorno;
+//    }
+//    
+//    public void SetterCantidad(int cantidad){
+//        this.cantidad=cantidad;
+//    }
+    
+    
+    
 }
