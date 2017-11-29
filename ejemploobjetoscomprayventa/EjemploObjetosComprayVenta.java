06f4f064e19a6bf250c91f18315be7c6c1629c9f
antonellapino1306 <31899215+antonellapino1306@users.noreply.github.com>
2017-11-29 14:42:45 -0300

    Update EjemploObjetosComprayVenta.java

--- ejemploobjetoscomprayventa/EjemploObjetosComprayVenta.java
+++ ejemploobjetoscomprayventa/EjemploObjetosComprayVenta.java
@@ -16,6 +16,30 @@ public class EjemploObjetosComprayVenta {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+     articulo miArt= new articulo("fanta", 666, 100, 3);
+       articulo miArt2= new articulo("sprite", 666, 80, 5);
+       articulo miArt3= new articulo("fanta", 666, 80, 28);
+        System.out.println("precio de venta: "+miArt.GetPrecioVenta());
+       
+       comercio ElBolicheDeTony;
+       ElBolicheDeTony=new comercio("tony");
+       
+       String respuesta;
+       respuesta=ElBolicheDeTony.ComprarArticulo(miArt, 3);
+        System.out.println(respuesta);
+       respuesta=ElBolicheDeTony.ComprarArticulo(miArt2, 8);
+        System.out.println(respuesta);
+       respuesta=ElBolicheDeTony.ComprarArticulo(miArt3, 8);
+        System.out.println(respuesta);
+        
+        //venta
+        respuesta=ElBolicheDeTony.VendrArticulo(miArt, 10);
+        System.out.println(respuesta);
+       
+    }
+    
+}
+    
     }
     
 }
